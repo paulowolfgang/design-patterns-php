c4568a44cdb1c20dbed7893e8c3f45d4ef6f2b83
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-11-06 17:32:13 -0300

    :sparkles: feat: Criada interface de integração

---
+++ Structural/Adapter/IErpAdapter.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Structural\Adapter;
+
+interface IErpAdapter
+{
+    public function generateToken(string $apiKey, string $user): string;
+    public function sendOrder(Order $order, string $token): bool;
+}
